12d9f7b39c74d42d8971e029f0c6e69839449440
Derek777 <vvcompany777@gmail.com>
2017-05-05 22:10:35 +0300

    Revert "qqq"
    This reverts commit 59c7253bff7129bd5e94138958f7ea59828cf707.

--- index.php
+++ index.php
@@ -3,5 +3,11 @@ error_reporting (E_ALL);
 session_start();
 require_once ('\startup.php');
 startup();
-jkjh
+
+
+echo "index" . '<br>';
+$ttt = new M_User();
+$rrr = new C_File();
+$mod = new M_User();
+
 echo"WWWWWWWWWWWW";
