59c7253bff7129bd5e94138958f7ea59828cf707
Derek777 <vvcompany777@gmail.com>
2017-05-05 22:09:26 +0300

    qqq

--- index.php
+++ index.php
@@ -3,11 +3,5 @@ error_reporting (E_ALL);
 session_start();
 require_once ('\startup.php');
 startup();
-
-
-echo "index" . '<br>';
-$ttt = new M_User();
-$rrr = new C_File();
-$mod = new M_User();
-
+jkjh
 echo"WWWWWWWWWWWW";
